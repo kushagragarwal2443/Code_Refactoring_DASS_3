5f9bdc5eeaf8ec07e76b7bf2a9a6de2c1529ef77
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-04-08 23:07:51 +0530

    Added Storage for Lane Event for feature 3

--- gameFiles/GameHistoryFile.java
+++ gameFiles/GameHistoryFile.java
@@ -14,20 +14,55 @@ public class GameHistoryFile {
     public static void addGame(Party party, int frame, int ball, Bowler bowler, int[][] cumulScore, HashMap score, int index, int frameNum, int[] curScores, boolean mechProblem)
             throws IOException {
 
+        String data = "\n- - - - - - - - - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - -\nMembers :\n";
+
         Vector Members = party.getMembers();
-        Vector<String> membersNick = new Vector<String>();
 
         for(int i = 0; i < Members.size(); i++) {
-            Bowler member = (Bowler) Members.get(i);
-            membersNick.add((String)member.getNickName());
+            Bowler member = (Bowler)Members.get(i);
+            data += member.getNickName() + '\t';
+            data += member.getFullName() + '\t';
+            data += member.getEmail() + '\t';
+            data += "\n";
         }
 
-        String currentBowler = bowler.getNickName();
+        data += "Frame : " + Integer.toString(frame) + "\n";
+        data += "Ball : "+Integer.toString(ball);
+        data += "\nCurrent Bowler : "+ bowler.getNickName() + "\t" + bowler.getFullName() + "\t" + bowler.getEmail();
 
+        data += "\nCumul Score : \n";
+
+        for(int i = 0; i < Members.size(); i++){
+            for(int j = 0;j < frameNum;j++){
+                data += Integer.toString(cumulScore[i][j]) +"\t";
+            }
+            data += '\n';
+        }
+
+        data += "\nScore :\n";
+
+        for(int i = 0;i < Members.size(); i++) {
+            int[] scores = (int[])score.get(Members.get(i));
+            for(int j = 0; j < 21;j++){
+                data += Integer.toString(scores[j]) + '\t';
+            }
+            data += "\n";
+        }
+
+        data += "Index : "+Integer.toString(index);
+        data += "\nFrame Number : "+Integer.toString(frameNum);
+        data += "\nCurrent Scores : ";
+
+        for (int i = 0;i < curScores.length;i++){
+            data += Integer.toString(curScores[i])+ "\t";
+        }
+
+        data += "\nProblem : " + mechProblem;
+        data += "\n- - - - - - - - - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - -\n";
 
         try (RandomAccessFile out = new RandomAccessFile(GAMEHISTORY_DAT, "rw")) {
             out.skipBytes((int) out.length());
-            //out.writeBytes(data);
+            out.writeBytes(data);
         } catch (Exception e) {
             System.out.println("Error Reading Game History File");
         }
